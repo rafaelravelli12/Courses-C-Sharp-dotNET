4c9bd948d8c76dd47b58b58f526978eb30f464da
Rafael D. Ravelli <rafaelravelli1@gmail.com>
2024-02-06 01:13:22 -0300

    02-Balta-io-Course-Fundamentos-do-Entity-Framework - Certificate updated ||||| 03-Course-Balta-io-Orientacao-a-Objetos-em-Projetos-Reais-com-C-sharp-e-dotNET-7 - Course in progress

---
+++ 03-Course-Balta-io-Orientacao-a-Objetos-em-Projetos-Reais-com-C-sharp-e-dotNET-7/UtmBuilder/UtmBuilder.Core/Utm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core
+{
+    public class Utm
+    {
+        public Utm(Url url, Campaign campaign)
+        {
+            Url = url;
+            Campaign = campaign;
+        }
+
+        public Url Url { get; }
+
+        public Campaign Campaign { get; }
+    }
+}
---
+++ 03-Course-Balta-io-Orientacao-a-Objetos-em-Projetos-Reais-com-C-sharp-e-dotNET-7/UtmBuilder/UtmBuilder.Core/ValueObjects/Url.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.ValueObjects
+{
+    public class Url : ValueObject
+    {
+        /// <summary>
+        /// Create a new URL
+        /// <param name="address">Address of URL (Website link)</param>
+        /// </summary>
+        public Url(string address)
+        {
+            Address = address;
+            InvalidUrlException.ThrowIfInvalid(address);
+        }
+
+        public string Address { get; }
+    }
+}
