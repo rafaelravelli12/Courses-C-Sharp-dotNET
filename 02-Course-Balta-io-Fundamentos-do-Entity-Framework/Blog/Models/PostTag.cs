0758c9935a944fe80794f634ead3aa92436af9cc
Rafael D. Ravelli <rafaelravelli1@gmail.com>
2024-02-21 01:06:08 -0300

    All projects - trying to ajust gitignore

--- 02-Course-Balta-io-Fundamentos-do-Entity-Framework/Blog/Models/PostTag.cs
+++
@@ -1,9 +0,0 @@
-namespace Blog.Models
-{
-    public class PostTag
-    {
-        public int PostId { get; set; }
-
-        public int TagId { get; set; }
-    }
-}
--- 06-Course-Microsoft-Training-Learning-Path-Develop-an-ASPdotNET-Core-web-app-that-consumes-an-API/FruitAPI/Fruit.cs
+++
@@ -1,7 +0,0 @@
-public class Fruit
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public bool Instock { get; set; }
-    
-}
